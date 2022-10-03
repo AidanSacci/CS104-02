0c48c4fbf4fa141e328e7d2fef308cb7f0bb5afa
AidanSacci <114097761+AidanSacci@users.noreply.github.com>
2022-10-03 12:43:03 -0400

    Average.py

--- Average.py
+++ Average.py
@@ -7,7 +7,7 @@ print("How many test scores would you like to average?")
 howMany=int(input(''))
 while howManyEntered<howMany:
     print("Enter test score:")
-    testScore=int(input(''))
+    testScore=float(input(''))
     total=total+testScore
     howManyEntered+=1
 average=total/howMany
